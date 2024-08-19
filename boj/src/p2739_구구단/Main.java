a8d96acd8eb1b9b50dafcf643ed0190ae4e70d7e
apple6346654 <apple6346654@naver.com>
2024-08-19 17:13:53 +0900

    p2739

---
+++ boj/src/p2739_구구단/Main.java
@@ -0,0 +1,17 @@
+package p2739_구구단;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N  = Integer.parseInt (br.readLine());
+
+        for (int i = 1; i<=9; i++){
+            System.out.println(N +" * "+ i +" = "+ N*i);
+        }
+    }
+}
