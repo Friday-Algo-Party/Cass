cbaedd373942624d74e73431f37d694d3101ceec
apple6346654 <apple6346654@naver.com>
2024-10-30 20:14:19 +0900

    helloworld

--- boj/src/Programmers/a와b출력/main.java
+++ boj/src/Programmers/a와b출력/main.java
@@ -1,8 +1,8 @@
-package Programmers;
+package Programmers.a와b출력;
 
 import java.util.Scanner;
 
-public class a와b출력 {
+public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
