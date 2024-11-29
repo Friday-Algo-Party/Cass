35330e4be745f6749fc6659a4752aec2d51b6baf
apple6346654 <apple6346654@naver.com>
2024-11-29 23:41:29 +0900

    수정

--- boj/src/Baekjoon/p10871_X보다작은수/Main.java
+++ boj/src/Baekjoon/p10871_X보다작은수/Main.java
@@ -22,8 +22,6 @@ public class Main {
         for(int i = 0; i < n; i++){
             if(arr[i] < x){
                 System.out.print(arr[i]+ " ");
-                System.out.print(arr[i]+ " ");
-                System.out.print(arr[i]+ " ");
             }
         }
 
