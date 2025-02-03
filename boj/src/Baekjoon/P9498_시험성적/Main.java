fbff98f079da03004d4bf3ba64ed397641fea28f
apple6346654 <apple6346654@naver.com>
2025-02-03 16:06:53 +0900

    p9498

--- boj/src/Baekjoon/P9498_시험성적/Main.java
+++ boj/src/Baekjoon/P9498_시험성적/Main.java
@@ -26,3 +26,32 @@ public class Main {
 
     }
 }
+
+
+//package Baekjoon.시험성적;
+//
+//import java.util.*;
+//        import java.io.*;
+//
+/////시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+////첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+//public class Main {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int a = Integer.parseInt(br.readLine());
+//
+//        if( a>=90 && a<=100) {
+//            System.out.println("A");
+//        }else if( a>= 80 && a<= 89){
+//            System.out.println("B");
+//        }else if( a>= 70 && a<= 79){
+//            System.out.println("C");
+//        }else if( a>= 60 && a<= 69){
+//            System.out.println("D");
+//        }else if( a>= 0 && a<= 59){
+//            System.out.println("F");
+//        }
+//
+//
+//    }
+//}
