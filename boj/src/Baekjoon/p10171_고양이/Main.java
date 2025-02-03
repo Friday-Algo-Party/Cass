482129ac123000deb4ece13344f1f8b8f78cbf7a
apple6346654 <apple6346654@naver.com>
2025-02-03 15:41:41 +0900

    p10171

---
+++ boj/src/Baekjoon/p10171_고양이/Main.java
@@ -0,0 +1,14 @@
+package Baekjoon.p10171_고양이;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\" +"    "+"/\\");
+        System.out.println(" )" +"  "+"( ')");
+        System.out.println("(" +"  "+"/  )");
+        System.out.println(" \\(__)|");
+    }
+}
+ㅔ
