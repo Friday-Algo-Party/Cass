31d9fd5f911b67d828b4a2c9abbc4e5a2f1f7f51
apple6346654 <apple6346654@naver.com>
2025-02-28 22:51:07 +0900

    p25083

---
+++ boj/src/Baekjoon/p25083_새싹/Main.java
@@ -0,0 +1,14 @@
+package Baekjoon.p25083_새싹;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" + 	// \" 이렇게 해야  " 출력 가능
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |";
+
+        System.out.print(s);
+    }
+}
