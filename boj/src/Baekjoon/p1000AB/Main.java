57c4c386f6e50cf1d22f8e25b5127baaf1dcb8ee
apple6346654 <apple6346654@naver.com>
2025-03-09 00:02:26 +0900

    p1000

--- boj/src/Baekjoon/p1000AB/Main.java
+++ boj/src/Baekjoon/p1000AB/Main.java
@@ -7,9 +7,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer("");
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
 
         System.out.println(A+B);
